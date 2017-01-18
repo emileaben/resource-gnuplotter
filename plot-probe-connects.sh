e836d720aa09bfb0308c98c164998548cfb23645
Stephen D. Strowes <sds@ripe.net>
2017-01-18 17:17:50 +0100

    hack the plot script into a bash script, add some boilerplate, change output canvas to pngcairo

--- plot-probe-connects.sh
+++ plot-probe-connects.sh
@@ -1,15 +1,52 @@
-set term pdf
+#!/usr/bin/env bash
+
+while getopts  "c:" flag
+do
+	case $flag in
+		c)
+			CC=$OPTARG
+			;;
+		h)
+			echo "$0 -c CC"
+			exit
+			;;
+	esac
+done
+
+if [ -z ${CC+x} ]
+then
+	echo "$0 -c CC"
+	exit 1
+fi
+
+if [ ! -e $CC.log ]
+then
+	echo "$CC.log doesn't exist!"
+	exit 1
+fi
+
+gnuplot <<EOF
+
+set term pngcairo size 1000,600
+
 set grid xtics
+set tics nomirror
+set border 3
+unset key
+
 set xdata time
 set timefmt "%s"
-set format x "%d%b\n%Hh"
+set format x "%d %b-%Hh"
+set xtics rotate
 unset ytics
-set output "NL.pdf"
+
 set title "RIPE Atlas probes in the Netherlands connected to RIPE Atlas infrastucture\n(only probes with disconnects shown)"
+
 set ylabel "Probes"
-#set yrange [-0.25:2.25]
-#set xrange ["1480118400":"1480622400"]
-#set xrange ["1480507200":]
 set xlabel "Time"
-unset key
-plot "NL.log" u 1:2:($3-$1):(0) w vectors nohead lc rgb "#4682b4"
+
+set output "$CC.png"
+plot "$CC.log" u 1:2:(\$3-\$1):(0) w vectors nohead lc rgb "#4682b4"
+
+EOF
+
